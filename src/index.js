5fccde8afa1da3df45f836d2406f51588da6c96e
lehai4 <86140878+lehai4@users.noreply.github.com>
2023-02-27 22:52:34 +0700

    use boostrap

---
+++ src/index.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
+
+const { engine } = require("express-handlebars");
+
+const app = express();
+const port = 3001;
+
+app.use(express.static(path.join(__dirname, "public")));
+//HTTP logger
+app.use(morgan("combined"));
+
+//Template engine
+app.engine(
+  "hbs",
+  engine({
+    extname: ".hbs",
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources/views"));
+
+app.get("/", (req, res) => {
+  return res.render("home");
+});
+
+app.get("/news", (req, res) => {
+  return res.render("news");
+});
+// 127.0.0.1
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
